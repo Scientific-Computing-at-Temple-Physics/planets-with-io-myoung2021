0a42c9ad397447f2e387b4edaf4f4d83b8685fa6
Mitchell Young <mitchellyoung@Mitchells-MacBook-Air.local>
2018-01-30 15:23:13 -0500

    Initial work on project!

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -1,4 +1,19 @@
-# This is a comment.  Python will ignore these lines (starting with #) when running
-
+# Mitchell Young
 import math as ma
-# To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
+
+data=open("planet_data.dat","r")
+
+name=str.capitalize(str.lower(raw_input("What is the name of the planet? Type help for options: ")))
+
+if name=="Help":
+    print "Mercury, Venus, Earth, Mars, Ceres, Jupiter, Saturn, Uranus, Neptune, Pluto, Eris, Comet, Proxima Centuari b, or Kepler-37b"
+
+"""
+alt=int(raw_input("What is your altitude?"))
+
+
+if line[0]==name:
+
+
+4/3*pi*R^3*P
+"""
